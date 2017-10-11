3967603742442a96e6a026446e20ec80c1bf2843
vagrant <vagrant@vagrant.vm>
2017-10-11 21:05:42 +0000

    WIP edge cases

--- reverse.py
+++ reverse.py
@@ -20,6 +20,21 @@ def rev_string(astring):
     You may NOT use the reversed() function!
     """
 
+    # w/out recursion, just convert str to lst, [::-1], convert back to str
+    # e.g. 'abc' --> c + 'ab' --> c + b + 'a' --> c + b + a + ''
+
+    # BASE
+        # when str empty, stop recursing
+    if len(astring) == 1:
+        return astring[0]
+
+    # PROGRESSION
+        # get last letter
+        # call fn w/ last letter removed
+
+    letter = astring[-1]
+    return letter + rev_string(astring[:-1])
+
 
 if __name__ == '__main__':
     import doctest
